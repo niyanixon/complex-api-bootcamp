97d4e259ad6023da8a3f7edeb459a58d03b26c61
N Nixon <ntnixon@Niyas-MacBook-Air.local>
2024-10-21 12:01:48 -0400

    changed project to a brewery website

--- main.js
+++ main.js
@@ -1,28 +1,30 @@
 
+addEventListener("DOMContentLoaded", (event) => {
+console.log('PAGE LOADED')
 
-
-document.querySelector('.foodbtn').addEventListener('click', findFood)
-
-function findFood(){
-    const userInput = document.querySelector('input').value
-    const url = `https://www.themealdb.com/api/json/v1/1/filter.php?i=${userInput}`
-    fetch(url)
-      .then(response => response.json())
-      //inputting an ingredient and its returning the first meal that comes up and putting it into the second api
-      .then(dataOne => {
-        return fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${dataOne.meals[0].strMeal}`)
-    })
-    .then(response => response.json())
-    // giving us the recipe name and the ingredients for the first meal from the first api link
-    .then(dataTwo => {
-        console.log(dataTwo)
-
-        document.querySelector('.name').innerText = dataTwo.meals[0].strMeal
-        document.querySelector('.picture').src = dataTwo.meals[0].strMealThumb
-        document.querySelector('.instructions').innerText = dataTwo.meals[0].strInstructions
-    })
-      .catch(error => {
-        console.log(`${error}`)
+const url = 'https://api.openbrewerydb.org/v1/breweries'
+fetch(url)
+  .then(response => response.json())
+  .then(data => {
+    data.forEach(element => {
+    const lat = element.latitude
+    const long = element.longitude
+    fetch(`https://currentuvindex.com/api/v1/uvi?latitude=${lat}&longitude=${long}`)
+  .then(response => response.json())
+  .then(dataTwo => {
+    console.log(dataTwo)
+   console.log(dataTwo.now.uvi)
+   document.querySelector('ul').innerHTML +=
+    `<li>Brewery Company: ${element.name}</br>State: ${element.state}</br>UVI: ${dataTwo.now.uvi}</br>Address: ${element.street}</br>Website: ${element.website_url}</li>`
     });
+  })
 
-}
+  .catch(error => {
+    console.log(`${error}`)
+  });
+
+  })
+  .catch(error => {
+    console.log(`${error}`)
+  });
+});
